a589db31ed1c0d2eeee66c3317cd9ceffeb5e3c1
Enio <mandosaldmer@gmail.com>
2019-06-14 19:42:24 -0300

    Configurando ESLint e editando package.json, algumas refatorações

--- aula-01/js/algoritmos.js
+++ aula-01/js/algoritmos.js
@@ -4,224 +4,223 @@ function propriedade3025(n) {
         const j = Math.floor(n % 100);
         return (i + j) * (i + j) == n;
     } else {
-        throw new Error('Argumento Inválido');
+        throw new Error("Argumento Inválido");
     }
 }
 
 function propriedade153(cdu) {
     if (100 <= cdu && cdu <= 999) {
-        c = parseInt(cdu / 100)
-        du = parseInt(cdu % 100)
-        d = parseInt(du / 10)
-        u = parseInt(du % 10)
-        return ((c * c * c) + (d * d * d) + (u * u * u) == cdu).toString()
+        let c = parseInt(cdu / 100);
+        let du = parseInt(cdu % 100);
+        let d = parseInt(du / 10);
+        let u = parseInt(du % 10);
+        return (c * c * c + d * d * d + u * u * u == cdu).toString();
     } else {
-        throw new RangeError('Valor fora da faixa');
+        throw new RangeError("Valor fora da faixa");
     }
 }
 
 function diaDaSemana(d, m, a) {
     if (1 <= d && d <= 31 && 1 <= m && m <= 12 && a > 1753) {
         if (m == 1 || m == 2) {
-            m = m + 12
-            a = a - 1
+            m = m + 12;
+            a = a - 1;
         }
-        s = d + (2 * m) + ((3 * (m + 1)) / 5) + a + (a / 4) - (a / 100) + (a / 400)
-        return parseInt(s % 7)
+        let s = d + 2 * m + (3 * (m + 1)) / 5 + a + a / 4 - a / 100 + a / 400;
+        return parseInt(s % 7);
     } else {
-        throw new Error('Argumento Inválido');
+        throw new Error("Argumento Inválido");
     }
 }
 
 function mod(x, y) {
     if (0 <= y && 0 < x) {
-        s = x
+        let s = x;
         while (y <= s) {
-            s = s - y
+            s = s - y;
         }
-        return s
+        return s;
     } else {
-        throw new Error('Argumento Inválido');
+        throw new Error("Argumento Inválido");
     }
 }
 
 function somaNaturais(n) {
     if (0 <= n) {
-        i = 2
-        s = 1
+        let i = 2;
+        let s = 1;
         while (i <= n) {
-            s = s + i
-            i = i + 1
+            s = s + i;
+            i = i + 1;
         }
 
-        return s
+        return s;
     } else {
-        throw new Error('Argumento Inválido');
+        throw new Error("Argumento Inválido");
     }
 }
 
 function fatorial(n) {
     if (1 <= n) {
-        i = 2
-        f = 1
+        let i = 2;
+        let f = 1;
         while (i <= n) {
-            f = f * i
-            i = i + 1
+            f = f * i;
+            i = i + 1;
         }
-        return f
+        return f;
     } else {
-        throw new Error('Argumento Inválido');
+        throw new Error("Argumento Inválido");
     }
 }
 
 function produto(a, b) {
     if (0 <= a && 0 <= b) {
-        totalParcelas = a
-        parcela = b
+        let totalParcelas = a;
+        let parcela = b;
         if (b < a) {
-            totalParcelas = b
-            parcela = a
+            totalParcelas = b;
+            parcela = a;
         }
-        let i = 1
-        let s = 0
+        let i = 1;
+        let s = 0;
         while (i <= totalParcelas) {
-            s += parcela
-            i += 1
+            s += parcela;
+            i += 1;
         }
-        return s
-
+        return s;
     } else {
-        throw new Error('Argumento Inválido');
+        throw new Error("Argumento Inválido");
     }
 }
 
 function potencia(x, y) {
     if (0 < x && 0 <= y) {
-        let potencia = 1
-        i = 1
+        let potencia = 1;
+        let i = 1;
         while (i <= y) {
-            potencia = produto(potencia, x)
-            i += 1
+            potencia = produto(potencia, x);
+            i += 1;
         }
-        return potencia
+        return potencia;
     } else {
-        throw new Error('Argumento Inválido');
+        throw new Error("Argumento Inválido");
     }
 }
 
 function pi(n) {
     if (1 <= n) {
-        i = 1
-        s = -1
-        d = -1
-        p = 0
+        let i = 1;
+        let s = -1;
+        let d = -1;
+        let p = 0;
         while (i <= n) {
-            d = d + 2
-            s = -1 * s
-            p = p + 4 * s / d
-            i = i + 1
+            d = d + 2;
+            s = -1 * s;
+            p = p + (4 * s) / d;
+            i = i + 1;
         }
-        return p
+        return p;
     } else {
-        throw new Error('Argumento Inválido');
+        throw new Error("Argumento Inválido");
     }
 }
 
 function logaritmoNatural(n, k) {
     if (1 <= n && 2 <= k) {
-        i = 2
-        e = 1 + n
-        numerador = 2
-        denominador = 1
+        let i = 2;
+        let e = 1 + n;
+        let numerador = 2;
+        let denominador = 1;
         while (i <= k) {
-            numerador = numerador * numerador
-            denominador = denominador * i
-            e = e + numerador / denominador
-            i = i + 1
+            numerador = numerador * numerador;
+            denominador = denominador * i;
+            e = e + numerador / denominador;
+            i = i + 1;
         }
         return e;
     } else {
-        throw new Error('Argumento Inválido');
+        throw new Error("Argumento Inválido");
     }
 }
 
 function razaoAurea(x, y, k) {
     if (0 <= x && x < y && 0 < k) {
-        c = y
-        a = x
-        i = 1
+        let c = y;
+        let a = x;
+        let i = 1;
         while (i <= k) {
-            t = c
-            c = c + a
-            a = t
-            i = i + 1
+            let t = c;
+            c = c + a;
+            a = t;
+            i = i + 1;
         }
-        return c / a
+        return c / a;
     } else {
-        throw new Error('Argumento Inválido');
+        throw new Error("Argumento Inválido");
     }
-
 }
 
 function quadradoPerfeito(n) {
     if (1 < n) {
-        i = 1
-        s = 1
+        let i = 1;
+        let s = 1;
         while (s < n) {
-            i += 2
-            s += i
+            i += 2;
+            s += i;
         }
-        return s == n
+        return s == n;
     } else {
-        throw new Error('Argumento Inválido');
+        throw new Error("Argumento Inválido");
     }
 }
 
 function raiz(n, i) {
     if (0 < n) {
-        r = 1
+        let r = 1;
         while (0 <= i) {
-            r = (r + n / r) / 2
-            i = i - 1
+            r = (r + n / r) / 2;
+            i = i - 1;
         }
-        return r
+        return r;
     } else {
-        throw new Error('Argumento Inválido');
+        throw new Error("Argumento Inválido");
     }
 }
 
 function primo(n) {
     if (n > 1) {
-        i = 2
+        let i = 2;
         while (i < n) {
             if (n % i == 0) {
-                return false
+                return false;
             }
-            i += 1
+            i += 1;
         }
-        return true
+        return true;
     } else {
-        throw new Error('Argumento Inválido');
+        throw new Error("Argumento Inválido");
     }
 }
 
 function crivoErastotenes(a, n) {
+    let i;
     for (i = 0; i < a.length; i++) {
-        if (n > 1 && !(i > 2 && i > n) || a[i] == 0) {
-            i = 2
-            limite = Math.abs(Math.sqrt(n))
+        if ((n > 1 && !(i > 2 && i > n)) || a[i] == 0) {
+            i = 2;
+            let limite = Math.abs(Math.sqrt(n));
             while (i <= limite) {
                 if (a[i] == 0) {
-                    multiplo = i + 1
+                    let multiplo = i + 1;
                     while (multiplo <= n) {
-                        a[multiplo] = 1
-                        multiplo = multiplo + 1
+                        a[multiplo] = 1;
+                        multiplo = multiplo + 1;
                     }
                 }
-                i = i + 1
+                i = i + 1;
             }
         } else {
-            throw new Error('Argumento Inválido');
+            throw new Error("Argumento Inválido");
         }
     }
     if (i == a.length + 1) {
@@ -234,13 +233,13 @@ function crivoErastotenes(a, n) {
 function mdc(a, b) {
     if (b < a && 0 < b) {
         while (b != 0) {
-            m = a % b
-            a = b
-            b = m
+            let m = a % b;
+            a = b;
+            b = m;
         }
-        return a
+        return a;
     } else {
-        throw new Error('Argumento Inválido');
+        throw new Error("Argumento Inválido");
     }
 }
 
@@ -248,77 +247,95 @@ function mdc2(a, b) {
     if (b < a && 0 < b) {
         while (a != b) {
             if (a > b) {
-                a = a - b
+                a = a - b;
             } else {
-                b = b - a
+                b = b - a;
             }
         }
-        return a
+        return a;
     } else {
-        throw new Error('Argumento Inválido');
+        throw new Error("Argumento Inválido");
     }
 }
 
 function horner(/*x, g, ag, ag-1, ..., a0*/) {
     if (1 <= arguments[1]) {
-        p = arguments[3]
-        i = arguments[1] - 1
+        let p = arguments[3];
+        let i = arguments[1] - 1;
         while (0 <= i) {
-            p *= arguments[0] + arguments[i + 2]
-            i = i - 1
+            p *= arguments[0] + arguments[i + 2];
+            i = i - 1;
         }
-        return p
+        return p;
     } else {
-        throw new Error('Argumento Inválido');
+        throw new Error("Argumento Inválido");
     }
 }
 
 function fibonacci(n) {
     if (0 <= n) {
-        a = 0
-        c = 1
+        let a = 0;
+        let c = 1;
         if (n == 0 || n == 1) {
-            return n
+            return n;
         }
-        i = 2
+        let i = 2;
         while (i <= n) {
-            t = c
-            c += a
-            a = t
-            i += 1
+            let t = c;
+            c += a;
+            a = t;
+            i += 1;
         }
-        return c
+        return c;
     } else {
-        throw new Error('Argumento Inválido');
+        throw new Error("Argumento Inválido");
     }
 }
 
 function cpf(d) {
     if (d.length == 11) {
-        j = d[0] + (2 * d[1]) + (3 * d[2]) + (4 * d[3]) + (5 * d[4]) + (6 * d[5]) + (7 * d[6]) + (8 * d[7]) + (9 * d[8])
-        k = d[1] + (2 * d[2]) + (3 * d[3]) + (4 * d[4]) + (5 * d[5]) + (6 * d[6]) + (7 * d[7]) + (8 * d[8]) + (9 * d[9])
-        dj = ((j % 11) % 10)
-        dk = ((k % 11) % 10)
-        return (dj == d[9] && dk == d[10])
+        let j =
+            d[0] +
+            2 * d[1] +
+            3 * d[2] +
+            4 * d[3] +
+            5 * d[4] +
+            6 * d[5] +
+            7 * d[6] +
+            8 * d[7] +
+            9 * d[8];
+        let k =
+            d[1] +
+            2 * d[2] +
+            3 * d[3] +
+            4 * d[4] +
+            5 * d[5] +
+            6 * d[6] +
+            7 * d[7] +
+            8 * d[8] +
+            9 * d[9];
+        let dj = (j % 11) % 10;
+        let dk = (k % 11) % 10;
+        return dj == d[9] && dk == d[10];
     } else {
-        throw new Error('Argumento Inválido');
+        throw new Error("Argumento Inválido");
     }
 }
 
 function cpf2(d) {
     if (d.length == 11) {
-        c = 8
-        p = d[8]
-        s = d[8]
+        let c = 8;
+        let p = d[8];
+        let s = d[8];
         while (0 <= c) {
-            p += d[c]
-            s += p
-            c -= 1
+            p += d[c];
+            s += p;
+            c -= 1;
         }
-        j = ((s % 11) % 10)
-        k = ((s - p + 9 * d[9]) % 11) % 10
-        return (j == d[9] && k == d[10])
+        let j = (s % 11) % 10;
+        let k = ((s - p + 9 * d[9]) % 11) % 10;
+        return j == d[9] && k == d[10];
     } else {
-        throw new Error('Argumento Inválido');
+        throw new Error("Argumento Inválido");
     }
 }
