07868bd2986ebc06ea446ef2d6c7412df8d31315
Enio <mandosaldmer@gmail.com>
2019-06-25 20:45:45 -0300

    Alterados nomes de variáveis para melhorar legibilidade

--- aula-07/vetor/java/vetor/src/main/main/java/com/github/eniomoura/ufg/cs/aula01/VetorUtils.java
+++ aula-07/vetor/java/vetor/src/main/main/java/com/github/eniomoura/ufg/cs/aula01/VetorUtils.java
@@ -17,44 +17,44 @@ public final class VetorUtils {
     private VetorUtils() {
     }
 
-    public static float menorTemperatura(final float[] temps) {
-        float rValue = temps[0];
-        for (int i = 1; i < temps.length; i++) {
-            if (rValue > temps[i]) {
-                rValue = temps[i];
+    public static float menorTemperatura(final float[] temperaturas) {
+        float returnValue = temperaturas[0];
+        for (int i = 1; i < temperaturas.length; i++) {
+            if (returnValue > temperaturas[i]) {
+                returnValue = temperaturas[i];
             }
         }
-        return rValue;
+        return returnValue;
     }
 
     public static int somaImpares(final int[] inteiros) {
-        int rValue = 0;
+        int returnValue = 0;
         for (int i = 0; i < inteiros.length; i++) {
             if (inteiros[i] % 2 == 1) {
-                rValue = rValue + inteiros[i];
+                returnValue = returnValue + inteiros[i];
             }
         }
-        return rValue;
+        return returnValue;
     }
 
     public static int numValoresIguais(final double[] numeros, final double igualA) {
-        int rValue = 0;
+        int returnValue = 0;
         for (int i = 0; i < numeros.length; i++) {
             if (numeros[i] == igualA) {
-                rValue++;
+                returnValue++;
             }
         }
-        return rValue;
+        return returnValue;
     }
 
 
     public static int numLetras(final String caracteres) {
-        int rValue = 0;
+        int returnValue = 0;
         for (int i = 0; i < caracteres.length(); i++) {
             if (Character.isLetter(caracteres.charAt(i))) {
-                rValue++;
+                returnValue++;
             }
         }
-        return rValue;
+        return returnValue;
     }
 }
