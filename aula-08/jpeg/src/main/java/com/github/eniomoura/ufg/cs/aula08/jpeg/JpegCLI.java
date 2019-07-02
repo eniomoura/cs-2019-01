9bfedd1874176ed52f7001f51e991a06b5a7d2f9
Enio <mandosaldmer@gmail.com>
2019-07-02 08:32:49 -0300

    Implementação da base do 'JPEG' da aula 08

--- aula-08/jpeg/src/main/java/com/github/eniomoura/ufg/cs/aula08/jpeg/JpegCLI.java
+++ aula-08/jpeg/src/main/java/com/github/eniomoura/ufg/cs/aula08/jpeg/JpegCLI.java
@@ -1,4 +1,17 @@
 package com.github.eniomoura.ufg.cs.aula08.jpeg;
 
+import java.io.IOException;
+
 public class JpegCLI {
+    public static void main(final String[] args) throws IOException {
+        validateArgs(args);
+        System.console().printf("%s\n", JpegController.readFile(args[0]));
+    }
+
+    public static void validateArgs(String[] args){
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Nenhum argumento foi "
+                    + "fornecido.");
+        }
+    }
 }
--- aula-08/jpeg/src/main/java/com/github/eniomoura/ufg/cs/aula08/jpeg/JpegController.java
+++ aula-08/jpeg/src/main/java/com/github/eniomoura/ufg/cs/aula08/jpeg/JpegController.java
@@ -1,4 +1,48 @@
 package com.github.eniomoura.ufg.cs.aula08.jpeg;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class JpegController {
+    public static final String FIRST_HEX = "ff";
+    public static final String SECOND_HEX = "d8";
+    public static final String PENULTIMATE_HEX = "ff";
+    public static final String LAST_HEX = "d9";
+
+    private static InputStream is;
+    private static String firstHex;
+    private static String secondHex;
+
+    public static boolean readFile(final String caminhoArquivo) throws IOException {
+        final File checkFile = new File(caminhoArquivo);
+        if (checkFile.length() == 0){
+            return false;
+        }
+        is = Files.newInputStream(Paths.get(caminhoArquivo));
+        firstHex = String.format("%02x", is.read());
+        secondHex = String.format("%02x", is.read());
+        return verifyBytes();
+    }
+
+    public static boolean verifyBytes() throws IOException{
+        if (firstHex.equals(FIRST_HEX)
+        && secondHex.equals(SECOND_HEX)) {
+            String penultimateHex = "";
+            String lastHex = "";
+            int nextHex = 0;
+            while ((nextHex = is.read()) != -1) {
+                penultimateHex = lastHex;
+                lastHex = String.format("%02x", nextHex);
+            }
+            if (penultimateHex.equals(PENULTIMATE_HEX)
+            && lastHex.equals(LAST_HEX)) {
+                is.close();
+                return true;
+            }
+        }
+        return false;
+    }
 }
