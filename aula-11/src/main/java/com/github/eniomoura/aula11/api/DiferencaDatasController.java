51b1df1f39c983c6dd8b0fe45eddf472befe744a
Enio <mandosaldmer@gmail.com>
2019-07-09 19:46:14 -0300

    Renomeadas variáveis e feitas alterações no javadoc

--- aula-11/src/main/java/com/github/eniomoura/aula11/api/DiferencaDatasController.java
+++ aula-11/src/main/java/com/github/eniomoura/aula11/api/DiferencaDatasController.java
@@ -5,55 +5,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 /** Função utilizada como lógica principal do Spring Boot. */
 @RestController
 public final class DiferencaDatasController {
 
     /** Requisição cross-origin efetuada no caminho '/ds' do servidor.
-     * Retorna a diferença entre duas datas, 'data1' e 'data2'.
-     * @param data1 Primeiro operando, uma data no formato 'dd/MM/aaaa'
-     *              para o cálculo da diferença.
-     * @param data2 Segundo operando, uma data no formato 'dd/MM/aaaa'
-     *              para o cálculo da diferença.
-     * @return A diferença entra as datas 'data1' e 'data2'.
+     * Utiliza o método localDateFromString para obter a data
+     * no formato LocalDate, e utiliza o método CalculateDifference
+     * para obter a diferença entre as datas.
+     * @param firstDate Primeiro operando, uma string no formato 'dd/MM/aaaa'
+     * para ser convertida em LocalDate e executada calculateDifference sobre.
+     * @param secondDate Segundo operando, uma string no formato 'dd/MM/aaaa'
+     * para ser convertida em LocalDate e executada calculateDifference sobre.
+     * @return Um objeto ResponseModel contendo a resposta de
+     * calculateDifference, a ser recebido pelo Spring Boot.
      */
     @CrossOrigin
     @GetMapping("ds")
-    public ResponseModel diferencaDatas(
-            @RequestParam("data1") final String data1,
-            @RequestParam("data2") final String data2) {
-            final LocalDate primeiraData = localDateFromString(data1);
-            final LocalDate segundaData = localDateFromString(data2);
-            if (primeiraData == null || segundaData == null) {
+    public ResponseModel calculateDifference(
+            @RequestParam("data1") final String firstDate,
+            @RequestParam("data2") final String secondDate) {
+            final LocalDate parsedFirstDate =
+                CalendarioUtils.localDateFromString(firstDate);
+            final LocalDate parsedSecondDate =
+                CalendarioUtils.localDateFromString(secondDate);
+            if (parsedFirstDate == null || parsedSecondDate == null) {
                 throw new IllegalArgumentException(
                     "É necessário passar duas datas no formato 'dd/MM/yyyy.");
             }
             return new ResponseModel(
-                CalendarioUtils.calculateDifference(primeiraData, segundaData));
-    }
-
-    /**
-     * Recupera a instância de {@link LocalDate} correspondente à sequência de
-     * caracteres.
-     *
-     * Função retirada e modificada a partir do recurso exemplo disponível em:
-     * https://github.com/kyriosdata/exemplo
-     *
-     * @param data Sequência de caracteres no formato dd-MM-yyyy.
-     *
-     * @return Instância de {@link LocalDate} ou {@code null},
-     * se a sequência não está no formato esperado (por exemplo, "01-01-2018")
-     */
-    public LocalDate localDateFromString(final String data) {
-        try {
-            final DateTimeFormatter fmt = DateTimeFormatter.ofPattern(
-                "dd/MM/yyyy");
-            return LocalDate.parse(data, fmt);
-        } catch (IllegalArgumentException | DateTimeParseException exp) {
-            return null;
-        }
+                CalendarioUtils.calculateDifference(
+                    parsedFirstDate, parsedSecondDate));
     }
 }
--- aula-11/src/test/java/com/github/eniomoura/aula11/api/DiferencaDatasControllerTest.java
+++ aula-11/src/test/java/com/github/eniomoura/aula11/api/DiferencaDatasControllerTest.java
@@ -10,28 +10,28 @@ public class DiferencaDatasControllerTest {
     @Test
     public void entradasIguaisController() {
         assertEquals(0 , 
-            ddc.diferencaDatas("08/07/2019", "08/07/2019").getDifference());
+            ddc.calculateDifference("08/07/2019", "08/07/2019").getDifference());
     }
 
     @Test
     public void diferencaPositivaController() {
         assertEquals(1 ,
-            ddc.diferencaDatas("08/07/2019", "09/07/2019").getDifference());
+            ddc.calculateDifference("08/07/2019", "09/07/2019").getDifference());
     }
 
     @Test
     public void diferencaNegativaController() {
         assertEquals(-1 ,
-            ddc.diferencaDatas("09/07/2019", "08/07/2019").getDifference());
+            ddc.calculateDifference("09/07/2019", "08/07/2019").getDifference());
     }
 
     @Test
     public void entradasInvalidasController() {
         assertThrows(IllegalArgumentException.class,
-            () -> ddc.diferencaDatas("", ""));
+            () -> ddc.calculateDifference("", ""));
         assertThrows(IllegalArgumentException.class,
-            () -> ddc.diferencaDatas("teste", "teste"));
+            () -> ddc.calculateDifference("teste", "teste"));
         assertThrows(IllegalArgumentException.class,
-            () -> ddc.diferencaDatas("99/99/9999", "99/99/9999"));
+            () -> ddc.calculateDifference("99/99/9999", "99/99/9999"));
     }
 }
