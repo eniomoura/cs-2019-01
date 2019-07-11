238ad74703f1b9cb628ab269271c924b55640abe
Enio <mandosaldmer@gmail.com>
2019-07-11 20:36:23 -0300

    Refatorando código

--- aula-12/src/main/java/com/github/eniomoura/aula12/api/ExtensoUtils.java
+++ aula-12/src/main/java/com/github/eniomoura/aula12/api/ExtensoUtils.java
@@ -1,227 +1,252 @@
 package com.github.eniomoura.aula12.api;
 
+import java.util.AbstractMap;
+import java.util.Map;
+
 /** Classe utilitária com funções de manipulação de datas. */
 public final class ExtensoUtils {
 
+    /** Dígito mais dominante do número inserido. */
     private static int milhar;
+    /** Segundo dígito mais dominante do número inserido. */
     private static int centena;
+    /** Terceiro dígito mais dominante do número inserido. */
     private static int dezena;
+    /** Quarto dígito mais dominante do número inserido. */
     private static int unidade;
+
+    /** Lista de nomes por extenso para unidades. */
+    private static final Map<Integer, String> UNIDADES = Map.of(
+        1, " um",
+        2, " dois",
+        3, " três",
+        4, " quatro",
+        5, " cinco",
+        6, " seis",
+        7, " sete",
+        8, " oito",
+        9, " nove"
+    );
+
+    /** Lista de nomes por extenso para dezenas. */
+    private static final Map<Integer, String> DEZENAS = Map.of(
+        2, " vinte",
+        3, " trinta",
+        4, " quarenta",
+        5, " cinquenta",
+        6, " sessenta",
+        7, " setenta",
+        8, " oitenta",
+        9, " noventa"
+    );
+
+    /** Lista de nomes por extenso para centenas. */
+    private static final Map<Integer, String> CENTENAS = Map.of(
+        1, " cento",
+        2, " duzentos",
+        3, " trezentos",
+        4, " quatrocentos",
+        5, " quinhentos",
+        6, " seissentos",
+        7, " setecentos",
+        8, " oitocentos",
+        9, " novecentos"
+    );
+
+    /** Lista de nomes por extenso para milhares. */
+    private static final Map<Integer, String> MILHARES = Map.of(
+        1, " mil",
+        2, " dois mil",
+        3, " três mil",
+        4, " quatro mil",
+        5, " cinco mil",
+        6, " seis mil",
+        7, " sete mil",
+        8, " oito mil",
+        9, " nove mil"
+    );
+
+    /**
+     * Lista de nomes por extenso para números com
+     * pronúncia por extenso diferentes do padrão.
+     */
+    private static final Map<Integer, String> ESPECIAIS = Map.ofEntries(
+        new AbstractMap.SimpleEntry<>(10, " dez"),
+        new AbstractMap.SimpleEntry<>(11, " onze"),
+        new AbstractMap.SimpleEntry<>(12, " doze"),
+        new AbstractMap.SimpleEntry<>(13, " treze"),
+        new AbstractMap.SimpleEntry<>(14, " quatorze"),
+        new AbstractMap.SimpleEntry<>(15, " quinze"),
+        new AbstractMap.SimpleEntry<>(16, " dezesseis"),
+        new AbstractMap.SimpleEntry<>(17, " dezessete"),
+        new AbstractMap.SimpleEntry<>(18, " dezoito"),
+        new AbstractMap.SimpleEntry<>(19, " dezenove"),
+        new AbstractMap.SimpleEntry<>(100, " cem")
+    );
 
     /** Construtor privado para explicitar classe utilitária. */
     private ExtensoUtils() {
     }
 
-    /** Método recebe um número inteiro, e retorna uma
-     * string com o número escrito por extenso.
+    /**
+     * Método recebe um número inteiro, e retorna uma string
+     * com o número escrito por extenso.
      * @param numero Um número inteiro.
      * @return Uma string com o número escrito em extenso.
      */
-    public static String writeInFull(final int numero){
+    public static String writeInFull(final int numero) {
         milhar = (numero / 1000) % 10;
         centena = (numero / 100) % 10;
         dezena = (numero / 10) % 10;
         unidade = numero % 10;
-        String extenso = "";
-        if(getEspeciais(numero)==null){
-            if(numero == 0){
+        if (getEspeciais(numero) == null) {
+            if (numero == 0) {
                 return "zero";
-            }else if (numero > 0 && numero < 10) {
-                extenso = getUnidade(numero);
-            }else if(numero < 100 && numero > 9) {
-                return assembleDoisDigitos(numero);
-            }else if (numero < 1000 && numero > 99) {
-                return assembleTresDigitos(numero);
-            }else if(numero < 10000 && numero>=1000){
-                return assembleQuatroDigitos(numero);
-            }else{
+            } else if (numero > 0 && numero < 10_000) {
+                return assembleString(numero);
+            } else {
                 throw new IllegalArgumentException(
                     "O número inserido deve estar entre 0 e 1000.");
-                }
-        }else{
-            return getEspeciais(numero);
-        }
-
-		return extenso;
-    }
-
-    public static String assembleDoisDigitos(final int numero){
-        if (unidade == 0) {
-            return getDezena(numero);
-        } else {
-            return getDezena(numero) + " e" + getUnidade(numero);
-        }
-    }
-
-    public static String assembleTresDigitos(final int numero){
-        if (dezena == 1) {
-            return getCentena(numero) + " e" + getEspeciais(numero%100);
-        }else if (dezena == 0) {
-            return getCentena(numero) + " e" + getUnidade(numero);
-        } else if (unidade == 0) {
-            if(getDezena(numero)==null){
-                return getCentena(numero) + " e" + getEspeciais(unidade);
-            }else{
-                return getCentena(numero) + " e" + getDezena(numero);
             }
         } else {
-            return getCentena(numero) + " e" + getDezena(numero) + " e"
-                + getUnidade(numero);
+            return getEspeciais(numero);
         }
     }
 
-    public static String assembleQuatroDigitos(final int numero){
-        if (centena == 0 && dezena == 0 && unidade == 0) { //2000
-            return getMilhar(numero);
-        } else if (getEspeciais(numero%1000)!=null) { //2100
-            return getMilhar(numero) + " e" + getEspeciais(numero%1000);
-        } else if (getEspeciais(numero%100)!=null) { //22
-            return getMilhar(numero) + getCentena(numero) + " e" + getEspeciais(numero%100);
-        } else if (centena == 0 && dezena == 0) { //2002
-            return getMilhar(numero) + " e" + getUnidade(numero);
-        } else if (centena == 0 && unidade == 0) { //2020
-            return getMilhar(numero) + " e" + getDezena(numero);
-        } else if (dezena == 0 && unidade == 0) { //2200
-            return getMilhar(numero) + " e" + getCentena(numero);
-        } else if (centena == 0) { //2022
-            return getMilhar(numero) + " e" + getDezena(numero) + " e" + getUnidade(numero);
-        } else if (dezena == 0) { //2202
-            return getMilhar(numero) + getCentena(numero) + " e" + getUnidade(numero);
-        } else if (unidade == 0) { //2220
-            return getMilhar(numero) + getCentena(numero) + " e" + getDezena(numero);
-        } else { //2222
-            return getMilhar(numero) + getCentena(numero) + " e" + getDezena(numero) + " e" + getUnidade(numero);
+    /**
+     * Método que monta a string a ser retornada pela requisição.
+     * @param numero o número a ser retornado por extenso.
+     * @return O número por extenso.
+     */
+    public static String assembleString(final int numero) {
+        final int rangeUnidades = 9;
+        final int rangeCentenas = 99;
+        final int mil = 1000;
+        if (numero <= rangeUnidades) {
+            return getUnidade();
+        } else if (numero <= rangeCentenas && numero > rangeUnidades) {
+            return assembleDoisDigitos(numero);
+        } else if (numero < mil && numero > rangeCentenas) {
+            return assembleTresDigitos(numero);
+        } else if (numero >= mil) {
+            return assembleQuatroDigitos(numero);
+        }
+        throw new IllegalArgumentException(
+            "O número inserido deve estar entre 0 e 1000.");
+    }
+
+    /**
+     * Método retorna um número por extenso, se ele tiver dois dígitos.
+     * @param numero o número de dois dígitos a ser retornado por extenso.
+     * @return o número por extenso.
+     */
+    public static String assembleDoisDigitos(final int numero) {
+        if (unidade == 0) {
+            return getDezena();
+        } else {
+            return getDezena() + " e" + getUnidade();
         }
     }
 
-    public static String getEspeciais(final int numero){
-        switch (numero) {
-        case 10:
-            return " dez";
-        case 11:
-            return " onze";
-        case 12:
-            return " doze";
-        case 13:
-            return " treze";
-        case 14:
-            return " quatorze";
-        case 15:
-            return " quinze";
-        case 16:
-            return " dezesseis";
-        case 17:
-            return " dezessete";
-        case 18:
-            return " dezoito";
-        case 19:
-            return " dezenove";
-        case 100:
-            return " cem";
-        default:
-            return null;
+    /**
+     * Método retorna um número por extenso, se ele tiver três dígitos.
+     * @param numero o número de três dígitos a ser retornado por extenso.
+     * @return o número por extenso.
+     */
+    public static String assembleTresDigitos(final int numero) {
+        final int um = 1;
+        if (dezena == um) {
+            return getCentena() + " e" + getEspeciais(numero % 100);
+        } else if (dezena == 0) {
+            if (unidade == 0) {
+                return getCentena();
+            } else {
+                return getCentena() + " e" + getUnidade();
+            }
+        } else if (unidade == 0) {
+            if (getDezena() == null) {
+                return getCentena() + " e" + getEspeciais(unidade);
+            } else {
+                return getCentena() + " e" + getDezena();
+            }
+        } else {
+            return getCentena() + " e" + getDezena() + " e" + getUnidade();
         }
     }
 
-    public static String getMilhar(final int numero){
-		switch (milhar) {
-		case 0:
-			return " ";
-		case 1:
-			return " mil";
-		case 2:
-			return " dois mil";
-		case 3:
-			return " três mil";
-		case 4:
-			return " quatro mil";
-		case 5:
-			return " cinco mil";
-		case 6:
-			return " seis mil";
-		case 7:
-			return " sete mil";
-		case 8:
-			return " oito mil";
-		case 9:
-			return " nove mil";
-        default:
-            return null;
-		}
-    }
-    
-    public static String getCentena(final int numero){
-		switch (centena) {
-		case 1:
-			return " cento";
-		case 2:
-			return " duzentos";
-		case 3:
-			return " trezentos";
-		case 4:
-			return " quatrocentos";
-		case 5:
-			return " quinhentos";
-		case 6:
-			return " seissentos";
-		case 7:
-			return " setecentos";
-		case 8:
-			return " oitocentos";
-		case 9:
-			return " novecentos";
-        default:
-            return null;
-		}
-    }
-    
-    public static String getDezena(final int numero){
-        switch (dezena) {
-        case 2:
-            return " vinte";
-        case 3:
-            return " trinta";
-        case 4:
-            return " quarenta";
-        case 5:
-            return " cinquenta";
-        case 6:
-            return " sessenta";
-        case 7:
-            return " setenta";
-        case 8:
-            return " oitenta";
-        case 9:
-            return " noventa";
-        default:
-            return null;
+    /**
+     * Método retorna um número por extenso, se ele tiver quatro dígitos.
+     * @param numero o número de quatro dígitos a ser retornado por extenso.
+     * @return o número por extenso.
+     */
+    public static String assembleQuatroDigitos(final int numero) {
+        if (centena == 0 && dezena == 0 && unidade == 0) {
+            return getMilhar();
+        } else if (getEspeciais(numero % 1000) != null) {
+            return getMilhar() + " e" + getEspeciais(numero % 1000);
+        } else if (getEspeciais(numero % 100) != null) {
+            return getMilhar() + getCentena() + " e"
+                + getEspeciais(numero % 100);
+        } else if (centena == 0 && dezena == 0) {
+            return getMilhar() + " e" + getUnidade();
+        } else if (centena == 0 && unidade == 0) {
+            return getMilhar() + " e" + getDezena();
+        } else if (dezena == 0 && unidade == 0) {
+            return getMilhar() + " e" + getCentena();
+        } else if (centena == 0) {
+            return getMilhar() + " e" + getDezena() + " e" + getUnidade();
+        } else if (dezena == 0) {
+            return getMilhar() + getCentena() + " e" + getUnidade();
+        } else if (unidade == 0) {
+            return getMilhar() + getCentena() + " e" + getDezena();
+        } else {
+            return getMilhar() + getCentena() + " e" + getDezena()
+                + " e" + getUnidade();
         }
     }
-    
-    public static String getUnidade(final int numero){
-        switch (unidade) {
-        case 0:
-            return " zero";
-        case 1:
-            return " um";
-        case 2:
-            return " dois";
-        case 3:
-            return " três";
-        case 4:
-            return " quatro";
-        case 5:
-            return " cinco";
-        case 6:
-            return " seis";
-        case 7:
-            return " sete";
-        case 8:
-            return " oito";
-        case 9:
-            return " nove";
-        default:
-            return null;
-        }
+
+    /**
+     * Método retorna o valor por extenso de um número não-padrão.
+     * @param numero número não padrão (11, 12, 100)
+     * @return o valor por extenso.
+     */
+    public static String getEspeciais(final int numero) {
+        return ESPECIAIS.get(numero);
+    }
+
+    /**
+     * Método retorna o valor por extenso de um componente módulo
+     * de 1000 do número processado.
+     * @return o valor por extenso.
+     */
+    public static String getMilhar() {
+        return MILHARES.get(milhar);
+    }
+
+    /**
+     * Método retorna o valor por extenso de um componente módulo
+     * de 100 do número processado.
+     * @return o valor por extenso.
+     */
+    public static String getCentena() {
+        return CENTENAS.get(centena);
+    }
+
+    /**
+     * Método retorna o valor por extenso de um componente módulo
+     * de 10 do número processado.
+     * @return o valor por extenso.
+     */
+    public static String getDezena() {
+        return DEZENAS.get(dezena);
+    }
+
+    /**
+     * Método retorna o valor por extenso do dígito menos significante do número
+     * processado.
+     * @return o valor por extenso.
+     */
+    public static String getUnidade() {
+        return UNIDADES.get(unidade);
     }
 }
